c0b539b954737a931e9a0c67411ab2cd879f7546
adewale <obadunadewale1234@gmail.com>
2024-03-24 13:31:03 +0100

    update name

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
+                <br />Coded by <a href="/">Alice</a>.
+            </p>
+        </footer>
+     );
+}
+ 
+export default Footer;
